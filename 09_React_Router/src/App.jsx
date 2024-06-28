da7afb0015bc6727772eb3898ab530fd17d43de4
Man Patel <man.mp07@gmail.com>
2024-06-28 19:22:35 +0530

    React Routing

---
+++ 09_React_Router/src/App.jsx
@@ -0,0 +1,12 @@
+import {Header, Footer, Home} from './components/'
+
+function App() {
+
+	return (
+		<>
+			
+		</>
+	)
+}
+
+export default App
---
+++ 09_React_Router/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header";
+import Home from "./Home";
+import Footer from "./Footer";
+import About from "./About"
+import Contact from "./Contact";
+
+export {Header, Home, Footer, About, Contact};
