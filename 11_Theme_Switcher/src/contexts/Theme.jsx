a2ff8842e4df56bcfbe33b3087ae1478e0dafc40
Man Patel <man.mp07@gmail.com>
2024-07-04 17:28:56 +0530

    Added Default value in createContext()

--- 11_Theme_Switcher/src/contexts/Theme.jsx
+++ 11_Theme_Switcher/src/contexts/Theme.jsx
@@ -1,6 +1,10 @@
 import { createContext, useContext } from "react";
 
-export const ThemeContext = createContext()
+export const ThemeContext = createContext({
+    themeMode : "dark",
+    lightMode : () => {},
+    darkMode : () => {}
+}) // As of now, Default values nhi daalenge to bhii chalega
 
 export default function useTheme(){
     return useContext(ThemeContext);
