197190441500198dae96202ccf69541c3254d24d
Man Patel <man.mp07@gmail.com>
2024-07-18 19:09:49 +0530

    Made Components

--- 14_AppWrite_MegaBlog/src/components/index.js
+++ 14_AppWrite_MegaBlog/src/components/index.js
@@ -1,2 +1,17 @@
-export {Header} from './Header/Header'
-export {Footer} from './Footer/Footer'
+import Header from "./Header/Header";
+import Footer from './Footer/Footer'
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from './container/Container'
+import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
+import PostCard from "./Postcard";
+import Select from "./Select";
+import Signup from "./SignUp";
+import Login from "./Login";
+import {Protected as AuthLayout} from "./AuthLayout";
+
+export {
+    Header, Footer, LogoutBtn, Container, Logo, Button, Input, PostCard, Select,
+    Signup, Login, AuthLayout
+}
