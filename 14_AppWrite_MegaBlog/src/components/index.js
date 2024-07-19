990672f7a6835c669aad18de5d9817be222241e5
Man Patel <man.mp07@gmail.com>
2024-07-19 19:16:14 +0530

    All Pages

--- 14_AppWrite_MegaBlog/src/components/index.js
+++ 14_AppWrite_MegaBlog/src/components/index.js
@@ -5,13 +5,15 @@ import Container from './container/Container'
 import Logo from "./Logo";
 import Button from "./Button";
 import Input from "./Input";
-import PostCard from "./Postcard";
+import PostCard from "./PostCard";
 import Select from "./Select";
-import Signup from "./SignUp";
+import Signup from "./Signup";
 import Login from "./Login";
-import {Protected as AuthLayout} from "./AuthLayout";
+import AuthLayout from "./AuthLayout";
+import RTE from './RTE'
+import PostForm from './PostForm/PostForm'
 
 export {
-    Header, Footer, LogoutBtn, Container, Logo, Button, Input, PostCard, Select,
-    Signup, Login, AuthLayout
+    Header, Footer, LogoutBtn, Container, Logo, Button, Input,
+    PostCard, Select, Signup, Login, AuthLayout, RTE, PostForm
 }
